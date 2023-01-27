c153ab66b889f46849ce5154911c7fe00dc45232
赵子健 <448708588@qq.com>
2023-01-27 22:18:17 +0800

    ETG

--- origin/show.py
+++ origin/show.py
@@ -1,10 +1,10 @@
 from ToSim import SimModel
 import numpy as np
 
-theMouse = SimModel("./models/dynamic_4l_t3.xml")
-path="./ctrldata/ground.npz"
-'''theMouse = SimModel("./models/ETG_obstacle1.xml")
-path="./ctrldata/obstacle1.npz"'''
+'''theMouse = SimModel("./models/dynamic_4l_t3.xml")
+path="./ctrldata/ground.npz"'''
+theMouse = SimModel("./models/ETG_obstacle1.xml")
+path="./ctrldata/obstacle1.npz"
 theMouse.initializing()
 
 ctrldata=np.load(path)
@@ -12,3 +12,4 @@ ctrldata=ctrldata['ctrldata']
 for i in range(len(ctrldata)//12):
     ctrlData=ctrldata[i*12:(i+1)*12]
     theMouse.runStep(ctrlData)
+theMouse.write_log()
